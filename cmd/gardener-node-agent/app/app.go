e4c958aee6e88d144b865187124059a15e4e32e7
edlimerkaj01 <edlimerkaj01@gmail.com>
2026-02-11 11:50:00 +0100

    minor changes

--- cmd/gardener-node-agent/app/app.go
+++ cmd/gardener-node-agent/app/app.go
@@ -179,7 +179,6 @@ func run(ctx context.Context, cancel context.CancelFunc, log logr.Logger, cfg *n
 	}
 
 	log.Info("Adding runnables to manager")
-
 	if err := mgr.Add(&controllerutils.ControlledRunner{
 		Manager: mgr,
 		BootstrapRunnables: []manager.Runnable{
